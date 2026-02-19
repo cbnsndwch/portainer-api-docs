00221e32cda4fdce32bc7da32b821aa0b6e811e9
Serge <serge@BEASTIE.tail410936.ts.net>
2026-02-19 00:59:29 +0000

    fix(layout): update repo link and site title

--- src/lib/layout.shared.tsx
+++ src/lib/layout.shared.tsx
@@ -16,14 +16,14 @@ const COLORS: Record<string, string> = {
 // fill this with your actual GitHub info, for example:
 export const gitConfig = {
     user: 'cbnsndwch',
-    repo: 'portainer-ce-api-docs',
+    repo: 'portainer-api-docs',
     branch: 'main'
 };
 
 export function baseOptions(): BaseLayoutProps {
     return {
         nav: {
-            title: 'Portainer CE API Docs'
+            title: 'Portainer API Docs'
         },
         githubUrl: `https://github.com/${gitConfig.user}/${gitConfig.repo}`
     };
